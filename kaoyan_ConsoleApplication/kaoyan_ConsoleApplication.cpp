07737287f89245ecb64096b4a62b53d1e2afcdf4
wei li <liweiliwei0103@outlook.com>
2018-05-21 10:10:13 +0800

    初次提交

--- kaoyan_ConsoleApplication/kaoyan_ConsoleApplication.cpp
+++ kaoyan_ConsoleApplication/kaoyan_ConsoleApplication.cpp
@@ -55,13 +55,15 @@ void BubbleSort(int k[], int n)
 	}
 }
 
-//选择排序
+//选择排序，通过n-i次关键字间的比较，从n-i+1个记录
+//中选出关键字最小的记录，并和第i（1<=i<=n）个记录交换。
 void SelectSort(int k[], int n)
 {
 	int i, j, min, temp;
 	for(i=0;i<n-1;i++)
 	{
 		min = i;
+		//找出最小元素的位置
 		for(j=i+1;j<n;j++)
 		{
 			if(k[j]<k[min])
@@ -81,12 +83,80 @@ void SelectSort(int k[], int n)
 	}
 }
 
+//调换两个元素的位置
+void swap(int k[], int low, int high)
+{
+	int temp;
+
+	temp = k[low];
+	k[low] = k[high];
+	k[high] = temp;
+}
+
+//返回一个新的基准点
+int Partition(int k[], int low, int high)
+{
+	int point;
+	point = k[low];
+	while(low<high)
+	{
+		//比较右边
+		while(low<high && k[high]>=point)
+		{
+			high--;
+		}
+		//跳出while循环，说明point的右边有元素比它小，需要移动（调换位置）
+		swap(k, low, high);
+
+		//比较左边
+		while (low<high && k[low] <= point)
+		{
+			low++;
+		}
+		//跳出while循环，说明point的左边有元素比它大，需要移动（调换位置）
+		swap(k, low, high);
+	}
+	//退出最外层while循环时，low与high相等
+	//返回中间点位置
+	return(low);
+}
+
+//快速排序，参数：数组，起始位置，结束位置
+//基本思想：
+//1）选择一个基准元素, 通常选择第一个元素或者最后一个元素,
+//2）通过一趟排序讲待排序的记录分割成独立的两部分，其中一部分记录的元素值均比基准元素值小。另一部分记录的 元素值比基准值大。
+//3）此时基准元素在其排好序后的正确位置
+//4）然后分别对这两部分记录用同样的方法继续进行排序，直到整个序列有序。
+void QSort(int k[], int low, int high)
+{
+	int point;//基准点
+	//会不断地对基准点进行比较，左右指针会移动
+	//当指针重叠的时候，说明已经进行了一轮比较
+	if(low<high)
+	{
+		//把中间点作为分界点
+		point = Partition(k, low, high);
+		
+		//递归调用
+		QSort(k, low, point - 1);//左边
+		QSort(k, point + 1, high);//右边
+	}
+}
+
+//这里作为外层函数，保持接口一致
+void QuickSort(int k[], int n)
+{
+	//参数：传入的数组，初始位置，最大的位置
+	QSort(k, 0, n-1);
+}
+
 int main()
 {
 	int a[5] = {5,2,3,4,2};
 	//InsertSort(a,5);
 	//BubbleSort(a,5);
-	SelectSort(a,5);
+	//SelectSort(a,5);
+	QuickSort(a,5);
     return 0;
 }
 
